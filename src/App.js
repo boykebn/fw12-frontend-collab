0180e42451f607c459e3f75b1147b7ff77bc98df
Cupu-Boy <fajrir357@gmail.com>
2022-12-19 11:31:01 +0700

    rollback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Home from "./pages/Home";
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import ResetPassword from "./pages/auth/ResetPassword";
-import ConfirmPassword from "./pages/auth/ConfirmPassword";
-import ConfirmRelogin from "./pages/auth/ConfirmRelogin";
-import PageReqResetPass from "./pages/auth/PageReqResetPass";
+import Home from "./pages/Home"
+import Sort from "./pages/Sort"
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
+import ResetPassword from "./pages/auth/ResetPassword"
+import ConfirmPassword from "./pages/auth/ConfirmPassword"
+import ConfirmRelogin from "./pages/auth/ConfirmRelogin"
+import PageReqResetPass from "./pages/auth/PageReqResetPass"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/sort" element={<Sort />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/reset-password" element={<ResetPassword />}></Route>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-const Sort = () => {
+const Home = () => {
   return (
     <div>
       <Navbar />
@@ -14,38 +14,20 @@ const Sort = () => {
             <div className="bg-white rounded-[6px]">
               <div className="pl-[20px] pr-[10px] py-[6px] flex items-center">
                 <div className="flex-1 text-[14px] text-[#9EA0A5] leading-[20px]">
-                  <input
-                    className="py-[19px] pl-[15px] pr-[380px]"
-                    placeholder="Search for any skill"
-                  />
+                  <input className="py-[19px] pl-[15px] pr-[530px]" placeholder="Search for any skill" />
                 </div>
                 <div className="flex items-center">
                   <div className="mr-[30px]">
-                    <img
-                      className=""
-                      src={require("../assets/images/search.png")}
-                      alt="search"
-                    />
+                    <img className="" src={require("../assets/images/search.png")} alt="search" />
                   </div>
                   <div className="mr-[25px]">
-                    <img
-                      className=""
-                      src={require("../assets/images/Line.png")}
-                      alt="Line"
-                    />
+                    <img className="" src={require("../assets/images/Line.png")} alt="Line" />
                   </div>
                   <div className="mr-[50px]">
-                    <select className="text-[#9EA0A5] text-[16px]">
-                      <option>Sort</option>
-                      <option>Sortir berdasarkan Skill</option>
-                      <option>Sortir berdasarkan Freelance</option>
-                      <option>Sortir berdasarkan Fulltime</option>
-                    </select>
+                    <span className="text-[#9EA0A5] leading-[20px] font-semibold">Kategori</span>
                   </div>
                   <div>
-                    <button className=" border-1 bg-[#5E50A1] py-[17px] px-[34px] rounded text-white font-semibold">
-                      Search
-                    </button>
+                    <button className=" border-1 bg-[#5E50A1] py-[17px] px-[34px] rounded text-white font-semibold">Search</button>
                   </div>
                 </div>
               </div>
@@ -55,100 +37,63 @@ const Sort = () => {
         <main className="px-[150px] pb-[50px]">
           <div className="bg-white flex pl-[20px] pt-[21px] pb-[38px] pr-[77px] mb-[1px] rounded-[8px]">
             <div className="mr-[35px] flex items-center">
-              <img
-                className="w-[100px] h-[100px]"
-                src={require("../assets/images/testi1.png")}
-                alt="testi1"
-              />
+              <img className="w-[100px] h-[100px]" src={require("../assets/images/testi1.png")} alt="testi1" />
             </div>
             <div className="flex-1">
               <div>
-                <span className="text-[#1F2A36] text-[22px] font-bold">
-                  Louis Tomlinson
-                </span>
+                <span className="text-[#1F2A36] text-[22px] font-bold">Louis Tomlinson</span>
               </div>
               <div className="mb-[6px]">
-                <span className="text-[#9EA0A5] text-[14px] leading-[20px]">
-                  Web developer - Freelance
-                </span>
+                <span className="text-[#9EA0A5] text-[14px] leading-[20px]">Web developer - Freelance</span>
               </div>
               <div className="flex items-center mb-[17px]">
-                <img
-                  className="mr-[15px] w-[16px] h-[16px]"
-                  src={require("../assets/images/map.png")}
-                  alt="map"
-                />
-                <span className="text-[#9EA0A5] text-[14px] leading-[20px]">
-                  Lorem ipsum
-                </span>
+                <img className="mr-[15px] w-[16px] h-[16px]" src={require("../assets/images/map.png")} alt="map" />
+                <span className="text-[#9EA0A5] text-[14px] leading-[20px]">Lorem ipsum</span>
               </div>
               <div>
-                <button className="border-1 bg-[#FBB01799] rounded-[4px] py-[5px] px-[23px] text-white">
-                  PHP
-                </button>
+                <button className="border-1 bg-[#FBB01799] rounded-[4px] py-[5px] px-[23px] text-white">PHP</button>
               </div>
             </div>
             <div className="flex justify-center items-center">
-              <button className="border-1 bg-[#5E50A1] py-[17px] px-[27px] rounded font-semibold text-white leading-[20px] text-[16px]">
-                Lihat Profile
-              </button>
+              <button className="border-1 bg-[#5E50A1] py-[17px] px-[27px] rounded font-semibold text-white leading-[20px] text-[16px]">Lihat Profile</button>
             </div>
           </div>
         </main>
         <div className="px-[150px] pb-[70px] text-[#9EA0A5] text-[18px] flex justify-center">
           <div className="mr-[14px]">
             <button>
-              <img
-                className="border-1 bg-white rounded py-[19px] px-[23px]"
-                src={require("../assets/images/left.png")}
-                alt="left"
-              />
+              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/left.png")} alt="left" />
             </button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              1
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">1</button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              2
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">2</button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              3
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">3</button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              4
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">4</button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              5
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">5</button>
           </div>
           <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">
-              6
-            </button>
+            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">6</button>
           </div>
           <div className="mr-[14px]">
             <button>
-              <img
-                className="border-1 bg-white rounded py-[19px] px-[23px]"
-                src={require("../assets/images/right.png")}
-                alt="right"
-              />
+              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/right.png")} alt="right" />
             </button>
           </div>
         </div>
       </div>
+
       <Footer />
     </div>
   );
 };
 
-export default Sort;
+export default Home;
