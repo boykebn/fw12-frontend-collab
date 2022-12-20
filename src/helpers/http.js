aa0ac26e822fe9db6f6cd1d674eec8f81b7555b3
Rio Prayoga <yoprayogap@gmail.com>
2022-12-20 23:40:01 +0700

    integrasi get profile details

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 const http = (token) => {
-    const headers = {}
-    if (token) {
-        headers.authorization = 'Bearer ' + token
-    }
-    const instance = axios.create({
-        baseURL: process.env.REACT_APP_URL_BACKEND,
-        headers
-    })
-    return instance
-}
+  const headers = {};
+  if (token) {
+    headers.authorization = "Bearer " + token;
+  }
+  const instance = axios.create({
+    baseURL: process.env.REACT_APP_URL_BACKEND,
+    headers,
+  });
+  return instance;
+};
 
-export default http
+export default http;
