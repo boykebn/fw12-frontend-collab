b022f96269ca820806a8e9e4ddb27a654a76eadc
arsrsyh <gotrany97@gmail.com>
2022-12-21 04:16:29 +0800

    Mengatur posisi notif

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,20 +27,20 @@ import React from "react";
 const Navbar = (props) => {
   return (
     <div>
-      <nav className="fixed pl-5 py-[33px]  md:pl-[150px] md:pr-[130px] bg-white w-full z-10">
+      <nav className="fixed px-5 py-[33px]  md:pl-[150px] md:pr-[130px] bg-white w-full z-10">
         <div className="container m-auto flex justify-between items-center text-gray-700">
           <img className="" src={require("../assets/images/navlogo.png")} alt="navlogo" />
-          <div className="flex items-center pr-0 text-base font-semibold">
-            <div className="group">
-              <button className="mr-[45px]">
+          <div className="flex items-center pr-0 text-base font-semibold gap-6 md:gap-12 lg:gap-12">
+            <div className="group border-[1px] border-black">
+              <button className="">
                 <img className="" src={require("../assets/images/bell.png")} alt="bell" />
               </button>
-              <div className="hidden pr-[200px] group-hover:block absolute">
-                <img className="text-left" src={require("../assets/images/notif.png")} alt="notif" />
+              <div className="hidden right-2 md:right-[165px] top-14 group-hover:block absolute border-[1px] border-black">
+                <img className="w-[258px] h-[305px] drop-shadow-lg ease-in duration-500 " src={require("../assets/images/notif.png")} alt="notif" />
               </div>
             </div>
             <div>
-              <button className="mr-[52px]">
+              <button>
                 <img className="" src={require("../assets/images/mail.png")} alt="mail" />
               </button>
             </div>
