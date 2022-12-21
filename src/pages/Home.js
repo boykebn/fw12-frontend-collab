a43829b71f7444a76ae96a599460779a3d318a23
arsrsyh <gotrany97@gmail.com>
2022-12-21 21:56:41 +0800

    responsive home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl"; // Arrow icon
+import { CiSearch } from "react-icons/ci"; // Search icon
 
 
 
@@ -28,25 +29,19 @@ const Home = () => {
     <div className="font-openSans">
       <Navbar />
       <Header />
-      <div className="bg-[#E5E5E5]">
+      <div className="bg-[#E5E5E5] ">
         <div className="px-10 py-[50px] md:px-[150px]">
-          <div className="">
-            <div className="bg-white rounded-[6px] hidden">
+          <div>
+            <div className="bg-white rounded-[6px] hidden lg:block w-max">
               <div className="pl-[20px] pr-[10px] py-[6px] flex items-center">
-                <div className="flex-1 text-[14px] text-[#9EA0A5] leading-[20px]">
+                <div className="flex-1 text-[14px] text-[#9EA0A5] leading-[20px] relative">
                   <input
-                    className="pr-48 py-[19px] pl-[15px] md:pr-[360px]"
+                    className=" py-[19px] pl-[15px] pr-14 w-[38rem] outline-1 outline-[#9EA0A5]"
                     placeholder="Search for any skill"
                   />
+                  <CiSearch className="absolute w-8 h-8 top-3 right-3 border-1 border-black"/>
                 </div>
                 <div className="md:flex items-center">
-                  <div className="mr-[30px]">
-                    <img
-                      className=""
-                      src={require("../assets/images/search.png")}
-                      alt="search"
-                    />
-                  </div>
                   <div className="mr-[25px]">
                     <img
                       className=""
@@ -55,8 +50,8 @@ const Home = () => {
                     />
                   </div>
                   <div className="mr-[50px]">
-                    <select className="text-[#9EA0A5] text-[16px]">
-                      <option>Sort</option>
+                    <select className="text-[#9EA0A5] text-[16px] bg-white outline-none">
+                      <option disabled>Kategori</option>
                       <option>Sortir berdasarkan Skill</option>
                       <option>Sortir berdasarkan Freelance</option>
                       <option>Sortir berdasarkan Fulltime</option>
@@ -72,28 +67,13 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="hidden pl-10 mb-5 justify-center items-center md:hidden">
-          <div className="mr-[50px]">
-            <select className="text-[#9EA0A5] text-[16px]">
-              <option>Sort</option>
-              <option>Sortir berdasarkan Skill</option>
-              <option>Sortir berdasarkan Freelance</option>
-              <option>Sortir berdasarkan Fulltime</option>
-            </select>
-          </div>
-          <div>
-            <button className=" border-1 bg-[#5E50A1] py-[17px] px-[34px] rounded text-white font-semibold">
-              Search
-            </button>
-          </div>
-        </div>
-        <main className="grid grid-cols-2 px-3 justify-center md:flex md:flex-col lg:flex lg:flex-col md:px-20 lg:px-[150px] pb-[50px] gap-5">
+        <main className="grid px-3 justify-center md:flex md:flex-col lg:flex lg:flex-col md:pl-10 md:pr-32 lg:pl-[110px] lg:pr-[170px] pb-[50px] gap-5 border-[1px] border-black">
           {profile?.results?.map((bio) => (
-            <div className="flex flex-col justify-center items-center gap-5 bg-white md:flex md:flex-row md:pl-[20px] md:pt-[21px] md:pb-[38px] md:pr-[20px] md:mb-[1px] rounded-[8px] lg:justify-start lg:pr-0">
+            <div className="flex flex-col justify-start pt-10 gap-10 mx-10 items-center bg-white w-80 md:w-full lg:w-full md:flex md:flex-row md:pl-[20px] md:pt-[21px] md:pb-[38px] md:pr-[20px] md:mb-[1px] rounded-[8px] lg:justify-start border-[1px] border-black">
               <div>
-                <img className=" md:w-[120px] md:h-[120px] md:rounded-[50%] lg:w-[120px] lg:h-[120px] lg:rounded-[50%]" src={require("../assets/images/testi1.png")} alt="testi1" />
+                <img className="w-28 md:w-[120px] md:h-[120px] md:rounded-[50%] lg:w-[120px] lg:h-[120px] lg:rounded-[50%] border-[1px] border-black" src={require("../assets/images/testi1.png")} alt="testi1" />
               </div>
-              <div className="flex flex-col justify-center items-center gap-5 md:gap-2 md:items-start lg:mr-64">
+              <div className="flex flex-col justify-center items-center gap-5 w-full md:w-[19rem] md:gap-2 md:items-start lg:w-[42rem] lg:p-2 border-[1px] border-black">
                 <div>
                   <span className="text-[#1F2A36] text-2xl md:text-[22px] font-bold">
                     {bio.name}
@@ -102,14 +82,14 @@ const Home = () => {
                 <div className="">
                   <span className="text-[#9EA0A5] text-[14px] leading-[20px] ">{bio.jobDesk}</span>
                 </div>
-                <div className="">
+                <div className="flex">
                   <img className="mr-[15px] w-[16px] h-[16px]" src={require("../assets/images/map.png")} alt="map" />
                   <span className="text-[#9EA0A5] text-[14px] leading-[20px]">{bio.address}</span>
                 </div>
-                <div className="grid md:grid-cols-3 gap-2">
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-2 justify-center items-center border-[1px] border-black">
                   {bio.skills.map((skill) => (
-                    <div className="w-max">
-                      <div className="border-1 bg-[#FBB01799] rounded-[4px] py-[5px] px-[23px] text-white">{skill}</div>
+                    <div>
+                      <div className="border-1 bg-[#FBB01799] rounded-[4px] px-2 py-[5px] text-white text-center">{skill}</div>
                     </div>
                   ))}
                 </div>
