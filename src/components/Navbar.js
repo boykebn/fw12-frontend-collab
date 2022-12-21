52bd602f94eca134e0d6fa68b46134e880e8250c
arsrsyh <gotrany97@gmail.com>
2022-12-21 15:05:10 +0800

    Responsive Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-// const Navbar = (props) => {
-//   return (
-//     <div>
-//       <div className="flex px-[150px] pt-[45px] pb-[34px]">
-//         <div className="flex-1">
-//           <img className="" src={require("../assets/images/navlogo.png")} alt="navlogo" />
-//         </div>
-//         <div className="flex items-center">
-//           <div className="mr-[45px]">
-//             <img className="" src={require("../assets/images/bell.png")} alt="bell" />
-//           </div>
-//           <div className="mr-[52px]">
-//             <img className="" src={require("../assets/images/mail.png")} alt="mail" />
-//           </div>
-//           <div>
-//             <img className="" src={require("../assets/images/profile.png")} alt="profile" />
-//           </div>
-//         </div>
-//       </div>
-//         <div className={props.blueBg}></div>
-//     </div>
-//   );
-// };
-
 const Navbar = (props) => {
   return (
     <div>
-      <nav className="fixed px-5 py-[33px]  md:pl-[150px] md:pr-[130px] bg-white w-full z-10">
+      <nav className="fixed px-5 py-[33px] md:px-20 lg:pl-[150px] lg:pr-[130px] bg-white w-full z-10">
         <div className="container m-auto flex justify-between items-center text-gray-700">
           <img className="" src={require("../assets/images/navlogo.png")} alt="navlogo" />
           <div className="flex items-center pr-0 text-base font-semibold gap-6 md:gap-12 lg:gap-12">
-            <div className="group border-[1px] border-black">
+            <div className="group">
               <button className="">
                 <img className="" src={require("../assets/images/bell.png")} alt="bell" />
               </button>
-              <div className="hidden right-2 md:right-[165px] top-14 group-hover:block absolute border-[1px] border-black">
+              <div className="hidden right-2 md:right-[165px] top-14 group-hover:block absolute">
                 <img className="w-[258px] h-[305px] drop-shadow-lg ease-in duration-500 " src={require("../assets/images/notif.png")} alt="notif" />
               </div>
             </div>
@@ -47,7 +23,7 @@ const Navbar = (props) => {
             </div>
             <div>
               <Link to="/edit-jobseeker-profile">
-                <img className="" src={require("../assets/images/profile.png")} alt="profile" />
+                <img className="w-[32px] h-[32px] rounded-[50%]" src={require("../assets/images/luffy.jpg")} alt="profile" />
               </Link>
             </div>
           </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import axios from "axios";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl"; // Arrow icon
+
+
 
 const Home = () => {
   const [profile, setProfile] = React.useState({});
@@ -26,12 +29,12 @@ const Home = () => {
       <div className="bg-[#E5E5E5]">
         <div className="px-10 py-[50px] md:px-[150px]">
           <div className="">
-            <div className="bg-white rounded-[6px]">
+            <div className="bg-white rounded-[6px] hidden">
               <div className="pl-[20px] pr-[10px] py-[6px] flex items-center">
                 <div className="flex-1 text-[14px] text-[#9EA0A5] leading-[20px]">
                   <input className="pr-48 py-[19px] pl-[15px] md:pr-[360px]" placeholder="Search for any skill" />
                 </div>
-                <div className="hidden md:flex items-center">
+                <div className="md:flex items-center">
                   <div className="mr-[30px]">
                     <img className="" src={require("../assets/images/search.png")} alt="search" />
                   </div>
@@ -46,9 +49,6 @@ const Home = () => {
                       <option>Sortir berdasarkan Fulltime</option>
                     </select>
                   </div>
-                  {/* <div className="mr-[50px]">
-                    <span className="text-[#9EA0A5] leading-[20px] font-semibold">Kategori</span>
-                  </div> */}
                   <div>
                     <button className=" border-1 bg-[#5E50A1] py-[17px] px-[34px] rounded text-white font-semibold">Search</button>
                   </div>
@@ -57,7 +57,7 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="pl-10 mb-5 flex justify-center items-center md:hidden">
+        <div className="hidden pl-10 mb-5 justify-center items-center md:hidden">
           <div className="mr-[50px]">
             <select className="text-[#9EA0A5] text-[16px]">
               <option>Sort</option>
@@ -66,95 +66,55 @@ const Home = () => {
               <option>Sortir berdasarkan Fulltime</option>
             </select>
           </div>
-          {/* <div className="mr-[50px]">
-            <span className="text-[#9EA0A5] leading-[20px] font-semibold">Kategori</span>
-          </div> */}
           <div>
             <button className=" border-1 bg-[#5E50A1] py-[17px] px-[34px] rounded text-white font-semibold">Search</button>
           </div>
         </div>
-        <main className="px-[150px] pb-[50px]">
+        <main className="grid grid-cols-2 px-3 justify-center md:flex md:flex-col lg:flex lg:flex-col md:px-20 lg:px-[150px] pb-[50px] gap-5">
           {profile?.results?.map((bio) => (
-            <div className="bg-white md:flex pl-[20px] pt-[21px] pb-[38px] pr-[77px] mb-[1px] rounded-[8px]">
-              <div className="mr-[35px] flex md:items-center">
-                <img className="md:w-[100px] md:h-[100px]" src={require("../assets/images/testi1.png")} alt="testi1" />
+            <div className="flex flex-col justify-center items-center gap-5 bg-white md:flex md:flex-row md:pl-[20px] md:pt-[21px] md:pb-[38px] md:pr-[20px] md:mb-[1px] rounded-[8px] lg:justify-start lg:pr-0">
+              <div>
+                <img className=" md:w-[120px] md:h-[120px] md:rounded-[50%] lg:w-[120px] lg:h-[120px] lg:rounded-[50%]" src={require("../assets/images/testi1.png")} alt="testi1" />
               </div>
-              <div className="flex-1">
+              <div className="flex flex-col justify-center items-center gap-5 md:gap-2 md:items-start lg:mr-64">
                 <div>
                   <span className="text-[#1F2A36] text-2xl md:text-[22px] font-bold">{bio.name}</span>
                 </div>
-                <div className="mb-[6px]">
-                  <span className="text-[#9EA0A5] text-[14px] leading-[20px]">{bio.jobDesk}</span>
+                <div className="w-[150px] text-center md:w-max">
+                  <span className="text-[#9EA0A5] text-[14px] leading-[20px] ">{bio.jobDesk}</span>
                 </div>
-                <div className="flex items-center mb-[17px]">
+                <div className="hidden md:flex items-center">
                   <img className="mr-[15px] w-[16px] h-[16px]" src={require("../assets/images/map.png")} alt="map" />
                   <span className="text-[#9EA0A5] text-[14px] leading-[20px]">{bio.address}</span>
                 </div>
-                <div className="flex">
+                <div className="grid md:grid-cols-3 gap-2">
                   {bio.skills.map((skill) => (
-                    <div className="w-max mr-3">
+                    <div className="w-max">
                       <div className="border-1 bg-[#FBB01799] rounded-[4px] py-[5px] px-[23px] text-white">{skill}</div>
                     </div>
                   ))}
                 </div>
                 <div className="mb-5 md:hidden"></div>
               </div>
-              <div className="flex justify-center items-center">
-                <button className="border-1 bg-[#5E50A1] py-[17px] px-[27px] rounded font-semibold text-white leading-[20px] text-[16px]">Lihat Profile</button>
+              <div className="flex justify-center items-center justify-self-end">
+                <button className="hidden md:block lg:block border-1 bg-[#5E50A1] py-[17px] md:px-3 lg:px-[27px] rounded font-semibold text-white leading-[20px] text-[16px] hover:scale-[1.05] ease-in duration-100">Lihat Profile</button>
               </div>
             </div>
           ))}
         </main>
-        <div className="hidden px-10 md:px-[150px] pb-[70px] text-[#9EA0A5] text-[18px] md:flex justify-center">
-          <div className="mr-[14px]">
-            <button>
-              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/left.png")} alt="left" />
-            </button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">1</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">2</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">3</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">4</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">5</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">6</button>
-          </div>
-          <div className="mr-[14px]">
-            <button>
-              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/right.png")} alt="right" />
-            </button>
-          </div>
-        </div>
-        <div className="px-10 md:px-[150px] pb-[70px] text-[#9EA0A5] md:text-[18px] flex justify-center md:hidden">
-          <div className="mr-[14px]">
-            <button>
-              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/left.png")} alt="left" />
-            </button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">1</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">2</button>
-          </div>
-          <div className="mr-[14px]">
-            <button className="border-1 bg-white rounded py-[15.5px] px-[24px]">3</button>
-          </div>
 
-          <div className="mr-[14px]">
-            <button>
-              <img className="border-1 bg-white rounded py-[19px] px-[23px]" src={require("../assets/images/right.png")} alt="right" />
-            </button>
+        <div className="flex px-10 md:px-[150px] pb-[70px] text-[#9EA0A5] text-[18px] md:flex justify-center gap-4">
+          <div className="flex justify-center items-center h-[58px] w-[58px] bg-white rounded-[4px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white">
+            <SlArrowLeft />
+          </div>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white">1</button>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white">2</button>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white">3</button>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white hidden md:block">4</button>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white hidden md:block">5</button>
+          <button className="border-1 bg-white rounded h-[58px] w-[58px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white hidden md:block">6</button>
+          <div className="flex justify-center items-center h-[58px] w-[58px] bg-white rounded-[4px] hover:scale-[1.05] hover:bg-[#5E50A1] hover:text-white">
+            <SlArrowRight />
           </div>
         </div>
       </div>
